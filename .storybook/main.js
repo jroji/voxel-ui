966812a1cf25d45f2ccc4ca5c68e2208e3f5dac1
jon.roji <jon.roji@sngular.com>
2021-10-01 19:51:28 +0200

    add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
+    "@storybook/addon-console"
+  ],
+  "core": {
+    "builder": "webpack5"
+  }
+}
